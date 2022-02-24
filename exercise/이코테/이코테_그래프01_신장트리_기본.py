e33f692d06a716a978117106407f9529b48a0581
kks <drather@naver.com>
2022-02-24 22:34:44 +0900

    그래프01 신장트리 기본문제 해설 추가

--- exercise/이코테/이코테_그래프01_신장트리_기본.py
+++ exercise/이코테/이코테_그래프01_신장트리_기본.py
@@ -1,3 +1,22 @@
+"""
+크루스칼 알고리즘
+
+크루스킬 알고리즘을 통해 최소 신장 트리를 구한다.
+다음 3가지의 메서드로 구성된다.
+
+1. find
+- 해당 노드의 부모 노드를 재귀 호출을 통해 찾아낸다.
+
+2. union
+- 두 가지 노드를 받아서, parent 를 설정한다.
+
+3. main
+- 간선들을 wgt 기준으로 정렬한다.
+- wgt 가 가장 작은 간선부터 신장트리에 포함시킨다. ( answer += wgt 하면서 )
+- 이 때, 사이클을 발생시키는 간선은 포함시키지 않는다. (if union(x1, n2): continue)
+"""
+
+
 import heapq
 
 
@@ -21,34 +40,25 @@ def union(p, n1, n2):
         return False
 
     else:
-        print("cycle")
         return True
 
 
-city_cnt, link_cnt = map(int, input().split())
-parents = [i for i in range(city_cnt+1)]
-links = []
-total_cost = 0
+if __name__ == '__main__':
+    city_cnt, link_cnt = map(int, input().split())
+    parents = [i for i in range(city_cnt+1)]
+    links = []
+    total_cost = 0
 
-for _ in range(link_cnt):
-    src, trg, wgt = map(int, input().split())
-    heapq.heappush(links, (wgt, src, trg))
+    for _ in range(link_cnt):
+        src, trg, wgt = map(int, input().split())
+        heapq.heappush(links, (wgt, src, trg))
 
+    for _ in range(link_cnt):
+        wgt, src, trg = heapq.heappop(links)
 
+        if union(parents, src, trg):
+            continue
 
-
-
-
-for _ in range(link_cnt):
-    wgt, src, trg = heapq.heappop(links)
-    print(f"src, trg, wgt: {src}, {trg}, {wgt}")
-    if union(parents, src, trg):
-        continue
-
-    else:
         total_cost += wgt
 
-print(parents[1:])
-print(total_cost)
-
-print([find(parents, i) for i in parents[1:] ])
+    print(total_cost)
