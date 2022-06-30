5f76e59576253c5d3ea76ab43cd8e5bf2458101b
kks <drather@naver.com>
2022-07-01 00:13:25 +0900

    자물쇠와 열쇠 풀이 실패

---
+++ exercise/이코테/실전문제/이코테12_구현04_자물쇠와열쇠.py
@@ -0,0 +1,110 @@
+"""
+문제 설명
+고고학자인 "튜브"는 고대 유적지에서 보물과 유적이 가득할 것으로 추정되는 비밀의 문을 발견하였습니다.
+그런데 문을 열려고 살펴보니 특이한 형태의 자물쇠로 잠겨 있었고
+문 앞에는 특이한 형태의 열쇠와 함께 자물쇠를 푸는 방법에 대해 다음과 같이 설명해 주는 종이가 발견되었습니다.
+
+잠겨있는 자물쇠는 격자 한 칸의 크기가 1 x 1인 N x N 크기의 정사각 격자 형태이고 특이한 모양의 열쇠는 M x M 크기인 정사각 격자 형태로 되어 있습니다.
+
+자물쇠에는 홈이 파여 있고 열쇠 또한 홈과 돌기 부분이 있습니다.
+열쇠는 회전과 이동이 가능하며 열쇠의 돌기 부분을 자물쇠의 홈 부분에 딱 맞게 채우면 자물쇠가 열리게 되는 구조입니다.
+자물쇠 영역을 벗어난 부분에 있는 열쇠의 홈과 돌기는 자물쇠를 여는 데 영향을 주지 않지만,
+자물쇠 영역 내에서는 열쇠의 돌기 부분과 자물쇠의 홈 부분이 정확히 일치해야 하며 열쇠의 돌기와 자물쇠의 돌기가 만나서는 안됩니다.
+또한 자물쇠의 모든 홈을 채워 비어있는 곳이 없어야 자물쇠를 열 수 있습니다.
+
+열쇠를 나타내는 2차원 배열 key 와 자물쇠를 나타내는 2차원 배열 lock 이 매개변수로 주어질 때,
+열쇠로 자물쇠를 열수 있으면 true 를, 열 수 없으면 false 를 return 하도록 solution 함수를 완성해주세요.
+
+제한사항
+key 는 M x M(3 ≤ M ≤ 20, M은 자연수)크기 2차원 배열입니다.
+lock 은 N x N(3 ≤ N ≤ 20, N은 자연수)크기 2차원 배열입니다.
+M은 항상 N 이하입니다.
+key 와 lock 의 원소는 0 또는 1로 이루어져 있습니다.
+0은 홈 부분, 1은 돌기 부분을 나타냅니다.
+
+입출력 예
+key	                                lock	                            result
+[[0, 0, 0], [1, 0, 0], [0, 1, 1]]	[[1, 1, 1], [1, 1, 0], [1, 0, 1]]	true
+
+23:00 ~ 23:30 문제풀이
+23:30 ~ 24:00 답지보고 풀이
+
+SOLUTION
+- 구현 문제이므로, 여러 조건들을 만족하게끔 구현한 뒤, 알맞게 실행한다.
+- 구현할 것
+1. 주어진 Key 를 시계방향으로 회전하는 메서드
+2. 상하좌우로 이동하는 메서드
+    - 상
+    - 하
+    - 좌
+    - 우
+
+
+- 시나리오
+1. 현재 상태에서,
+
+"""
+
+
+# 2차원 리스트 90도 회전하기
+def rotate_a_matrix_by_90_degree(a):
+    n = len(a) # 행 길이 계산
+    m = len(a[0]) # 열 길이 계산
+    result = [[0] * n for _ in range(m)] # 결과 리스트
+    for i in range(n):
+        for j in range(m):
+            result[j][n - i - 1] = a[i][j]
+    return result
+
+
+# 자물쇠의 중간 부분이 모두 1인지 확인
+def check(new_lock):
+    lock_length = len(new_lock) // 3
+    for i in range(lock_length, lock_length * 2):
+        for j in range(lock_length, lock_length * 2):
+            if new_lock[i][j] != 1:
+                return False
+    return True
+
+
+def solution(key, lock):
+    n = len(lock)
+    m = len(key)
+    # 자물쇠의 크기를 기존의 3배로 변환
+    new_lock = [[0] * (n * 3) for _ in range(n * 3)]
+    # 새로운 자물쇠의 중앙 부분에 기존의 자물쇠 넣기
+    for i in range(n):
+        for j in range(n):
+            new_lock[i + n][j + n] = lock[i][j]
+
+    # 4가지 방향에 대해서 확인
+    for _ in range(4):
+        key = rotate_a_matrix_by_90_degree(key) # 열쇠 회전
+        for x in range(n * 2):
+            for y in range(n * 2):
+                # 자물쇠에 열쇠를 끼워 넣기
+                for i in range(m):
+                    for j in range(m):
+                        new_lock[x + i][y + j] += key[i][j]
+                # 새로운 자물쇠에 열쇠가 정확히 들어 맞는지 검사
+                if check(new_lock):
+                    return True
+                # 자물쇠에서 열쇠를 다시 빼기
+                for i in range(m):
+                    for j in range(m):
+                        new_lock[x + i][y + j] -= key[i][j]
+    return False
+
+
+if __name__ == '__main__':
+    k = [[0, 0, 0],
+         [1, 0, 0],
+         [0, 1, 1]]
+
+    l = [[1, 1, 1],
+         [1, 1, 0],
+         [1, 0, 1]]
+
+    print(solution(k,l))
+
+
